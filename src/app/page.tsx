2b5186db4a81320a387d66174d15235d1ff63960
Mohi Bagherani <mohammad.bagherani@gmail.com>
2024-09-20 19:10:34 +0200

    feat: breaking the pipeline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
     <div>
-      <h1 role="home-title">Home</h1>
+      <h1 role="home-title">Home after about</h1>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h1 role="about-title">About</h1>
     </div>
   );
 }
