cbad955f31fd7b5d036e13eddece2aa6baf5d8b2
Mohi Bagherani <mohammad.bagherani@gmail.com>
2024-09-20 11:43:02 +0200

    feat: jest added

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+const nextJest = require('next/jest')
+
+/** @type {import('jest').Config} */
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
+}
+
+module.exports = createJestConfig(config)
