3ad7acae46e36b51f722715695e6268d2e47e703
Mohi Bagherani <mohammad.bagherani@gmail.com>
2024-09-20 19:19:29 +0200

    feat: home page changes reverted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 role="home-title">Home after about</h1>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h1 role="about-title">About</h1>
+      <h1 role="home-title">Home</h1>
     </div>
   );
 }
